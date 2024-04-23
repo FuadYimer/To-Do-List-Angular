4a25ff7f26d0d9189f1df07b3a1332eb6ed8b209
FuadYimer <36766534+FuadYimer@users.noreply.github.com>
2024-04-23 16:07:24 +0300

    To Do Taks Firs Draft

---
+++ src/app/todolist/todolist.component.ts
@@ -0,0 +1,45 @@
+
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-todolist',
+  standalone: true,
+  imports: [FormsModule, CommonModule],
+  templateUrl: './todolist.component.html',
+  styleUrl: './todolist.component.css'
+})
+export class TodolistComponent {
+
+  taskArray = [{ taskName: 'Brush teeth', isCompleted: false }];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form);
+
+    this.taskArray.push({
+      taskName: form.controls['task'].value,
+      isCompleted: false
+    })
+
+    form.reset();
+  }
+
+  onDelete(index: number) {
+    console.log(index);
+
+    this.taskArray.splice(index, 1);
+  }
+
+  onCheck(index: number) {
+    console.log(this.taskArray);
+
+    this.taskArray[index].isCompleted = !this.taskArray[index].isCompleted;
+  }
+
+}
